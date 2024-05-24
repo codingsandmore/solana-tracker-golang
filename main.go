f30fa59aad4ed22be2461abd671b7807dae9a383
Gert Wohlgemuth <berlinguyinca@gmail.com>
2024-05-24 14:07:02 -0700

    Updated swap transaction parameters in main.go and README
    An additional parameter has been added to the GetSwapTransaction function in the main.go file and the relevant change has been reflected in the README documentation too. This parameter corresponds to a new rate argument in the swap transaction.

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	rate, err := swap.GetRate("So11111111111111111111111111111111111111112", "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R", 10, 15)
 
 	//get the swap transaction
-	tx, err := swap.GetSwapTransaction("So11111111111111111111111111111111111111112", "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R", 10, 15, "44ByXN2NgCeHJdsmbUvKrjcdRQJLJDuY8DqnqWMcwy5U")
+	tx, err := swap.GetSwapTransaction("So11111111111111111111111111111111111111112", "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R", 10, 15, "44ByXN2NgCeHJdsmbUvKrjcdRQJLJDuY8DqnqWMcwy5U", 0.015)
 
 	if err != nil {
 		log.Fatal(err)
